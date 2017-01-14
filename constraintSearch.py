726874a0cb4a9713940105698207ab84f6719edd
Manuel Xarez <manuelxarez@ua.pt>
2017-01-14 14:18:26 +0000

    Created some constraint functions and scheduler is creating domain

--- constraintSearch.py
+++ constraintSearch.py
@@ -1,5 +1,12 @@
 def ConstraintSearch:
-    def __init__(self):
+    def __init__(self,domain,constraints):
+        #Domain is a dictionary with var as key and a list of values as value
+        self.domain = domain
+        #Constraints is a list of functions which require domain as the argument
+        self.constraints = constraints
 
     def search(self):
-        
+        #Returns none if there is no solution
+        if any([ domain[key] == [] for key in domain.keys()]):
+            return None
+        pass
--- scheduler.py
+++ scheduler.py
@@ -1,4 +1,18 @@
-def SubTypeConstraint(events):
+from event import Event
+from date import Date
 
-def Scheduler():
-            
+#Returns True if there are different subtypes for a type
+def SubTypeConstraint(domain):
+    for key in domain.keys():
+        if len(set([ e.st for e in domain[key])) != 1:
+                return True
+    return False
+
+#Returns True if there is any date overlap in the domain
+def overlapConstraint(domain):
+    pass
+
+def Scheduler(events):
+    domain = { e.t: [] for e in events}
+    for e in events:
+        domain[e.t] += [e]
