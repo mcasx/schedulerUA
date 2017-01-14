e27cc8e683e99b2de0247de30a5855ac814e70fb
Manuel Xarez <manuelxarez@ua.pt>
2017-01-14 13:23:50 +0000

    Event structure defined

---
+++ date.py
@@ -0,0 +1,5 @@
+class Date:
+    def __init__(self,weekDay,hour,minute):
+        self.weekDay = weekDay
+        self.hour = hour
+        self.minute = minute
---
+++ event.py
@@ -0,0 +1,8 @@
+class Event:
+    def __init__(self,startDate,duration,t,st):
+        self.startDate = startDate
+        self.duration = duration
+        #self.t is type of event, pe: AC1
+        self.t = t
+        #self.st is subtype of event, pe: TP1
+        self.st = st
