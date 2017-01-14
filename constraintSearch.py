65bda4a342472be0b6e77e64bffd83b42310044a
Manuel Xarez <manuelxarez@ua.pt>
2017-01-14 15:26:10 +0000

    Initial constraint search defined

--- constraintSearch.py
+++ constraintSearch.py
@@ -6,7 +6,7 @@ class ConstraintSearch:
         self.constraints = constraints
 
     def search(self,domain = None):
-        if domain = None:
+        if domain == None:
             domain = self.domain
         #Returns none if there is no solution
         if any([ domain[key] == [] for key in domain.keys()]):
@@ -14,3 +14,20 @@ class ConstraintSearch:
         #Returns solution if found
         if all([ len(domain[key]) == 1 for key in domain.keys() ]):
             return domain
+
+        #Orders keys in order of increasing length of domain
+        keys = sorted(domain.keys(), key = len(domain[key]))
+        #Missing picking value by most constraints created
+        nDomain = dict(domain)
+        for key in keys:
+            if len(nDomain[key]) > 1:
+                for value in nDomain[key]:
+                    #Pick a value
+                    nDomain[key] = [value]
+                    #Other variables values are the ones that are not constrained by picked value
+                    for x in [ x for x in keys if x != key ]:
+                        nDomain[x] = [ x for x in nDomain[x] if not any([ y(nDomain[key],nDomain[x]) for y in self.constraints ]) ]
+                    solution = self.search(nDomain)
+                    if solution != None:
+                        return solution
+        return None
--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,11 @@
 from event import Event
 from date import Date
 
-#Returns True if there is any date overlap in the domain
-def OverlapConstraint(domain):
-    events = [ e for key in domain.keys() for c in domain[key] for e in c.events ]
-    for e1 in events:
-        for e2 in [ e for e in events if e.weekDay == e1.weekDay and e1.startDate.minutify() <= e.startDate.minutify()]:
-            if e1.startDate.minutify() + e1.duration > e.startDate.minutify():
+#Returns True if there is any event in class1 overlaps class2
+def OverlapConstraint(class1,class2):
+    for e1 in class1.events:
+        for e2 in [ e for e in class2.events if e.weekDay == e1.weekDay and e1.startDate.minutify() <= e.startDate.minutify()]:
+            if e1.startDate.minutify() + e1.duration > e2.startDate.minutify():
                 return True
     return False
 
